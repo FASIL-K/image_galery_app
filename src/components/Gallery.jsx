c1aea45c09eb4a78eaf5342bf428706c32e9b0d7
FASIL K <fasilmlm10@gmail.com>
2023-10-02 20:05:59 +0530

    Successfully done image gallery app

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,23 +1,82 @@
-import React from 'react'
+import React, { useState,useEffect } from 'react'
 import './Gallery.css'
+
+
 function Gallery() {
+const handleDeleteImage=(id)=>{
+    setImages(images.filter((i)=>i.id !== id))
+    
+}
+const [images,setImages]=useState([])
+
+useEffect(()=>{
+
+const fetchImages=()=>{
+        
+    const data= [
+    {
+    id:1,
+    url:'https://picsum.photos/id/1/300/200' ,
+    title:'Image 1'
+
+    },
+    {
+    id:2,
+    url:' https://picsum.photos/id/2/300/200',
+    title:'image2'
+    },
+    {
+    id:3,
+    url:' https://picsum.photos/id/3/300/200',
+    title:'image3'
+    },
+    {
+    id:4,
+    url:' https://picsum.photos/id/4/300/200',
+    title:'image4'
+    },
+    {
+    id:5,
+    url:' https://picsum.photos/id/5/300/200',
+    title:'image5'
+    },
+
+
+    ]
+
+    setImages(data)
+    }
+    fetchImages()
+},[])
+
+
   return (
     <div>
         <h1 className='title'> Image Gallery App </h1>
-      <div className='image-grid'>
-        <div className='image-card'>
+      {
+        images.length >0 ?
 
-            <img src="" alt="" />
+        <div className='image-grid'>
+        {
+        images.map((image)=>(
+
+        <div className='image-card' key={image.id}>
+
+            <img src={image.url} alt="" />
 
             <div className='image-details'>
-                <h3>image1</h3>
-                <button>Delete</button>
+                <h3> {image.title} </h3>
+                <button onClick={()=>handleDeleteImage(image.id)} >Delete</button>
 
             </div>
 
         </div>
+            ))
+
+        }
 
       </div>
+      : <p style={{display:'flex',  fontSize:'24px', justifyContent:'center'}} >No Images</p> }
     </div>
   )
 }
